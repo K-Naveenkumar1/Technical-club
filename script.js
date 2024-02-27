54546c90661bd9786c03378919700b98c92e659b
K.NAVEEN KUMAR <156052184+K-Naveenkumar1@users.noreply.github.com>
2024-02-27 22:24:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+window.addEventListener("load", () =>{
+  const loader =document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader .addEventListener("transitionend", () => {
+    document.body.removeChild("loader");
+  })
+})
 'use strict';
 
 
